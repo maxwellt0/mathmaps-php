7d42d6b2cd720b77121baafcfdc6f534ce10e1fb
Maksym Vynnyk <maksimvinnik@gmail.com>
2015-09-10 00:36:30 +0300

    pass chapter 7

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -63,9 +63,8 @@ class SiteController extends Controller
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
-
         $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+        if ($model->load(Yii::$app->request->post()) && $model->loginAdmin()) {
             return $this->goBack();
         } else {
             return $this->render('login', [
