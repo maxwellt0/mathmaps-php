8e050b07e248ed7656c275c7cda9b5583c9a160b
Maksym Vynnyk <maksimvinnik@gmail.com>
2015-09-23 23:45:27 +0300

    pass 10 chapter

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,38 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'modules' => [
+        'social' => [
+// the module class
+            'class' => 'kartik\social\Module',
+// the global settings for the disqus widget
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // default settings
+            ],
+// the global settings for the facebook plugins widget
+            'facebook' => [
+                'appId' => '984458064938947',
+                'secret' => '82964a63754f41207ba9372b5091bd57',
+            ],
+// the global settings for the google plugins widget
+            'google' => [
+                'clientId' => 'GOOGLE_API_CLIENT_ID',
+                'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+                'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+            ],
+// the global settings for the google analytic plugin widget
+            'googleAnalytics' => [
+                'id' => 'TRACKING_ID',
+                'domain' => 'TRACKING_DOMAIN',
+            ],
+// the global settings for the twitter plugin widget
+            'twitter' => [
+                'screenName' => 'TWITTER_SCREEN_NAME'
+            ],
+        ],
+// your other modules
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
